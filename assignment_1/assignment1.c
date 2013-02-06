559c6edd306ccc86a404f6be603c3d57c563bc37
Snuggert <abe.wiersma@hotmail.nl>
2013-02-06 19:26:38 +0100

    More updates

--- assignment_1/assignment1.c
+++ assignment_1/assignment1.c
@@ -19,32 +19,49 @@ int main( int argc, char *argv[] )
 	unsigned long long randomNumbers;
 	long double fourthPI, myPI;
 
-	if(atoi( (argv[1]) ) >= 0)
+	if(argc > 0)
 	{
-		randomNumbers = atoi( (argv[1]) );
-		randomNumbersInside = 0;
+		if(atoi( (argv[1]) ) >= 0)
+		{
+			randomNumbers = atoi( (argv[1]) );
+			randomNumbersInside = 0;
 
-		printf("\nNumbers to be generated: %llu \n", randomNumbers);
+			printf("\nNumbers to be generated: %llu \n", randomNumbers);
 
-		for (index=0; index<randomNumbers; index++) {
-			long double x, y, lengthxy;
-			x = getRandom();
-			y = getRandom();
-			lengthxy = sqrt(pow(x, 2.0)+ pow(y, 2.0));
-			if(lengthxy <= 1.0){
-				randomNumbersInside ++;
+			/*
+			 * A random number is chosen the number of times the user defines.
+			 * If the random number is located within a radius of 1/4th of a
+			 * circle, randomNumbersInside is incremented.
+			 */
+			for (index=0; index<randomNumbers; index++) {
+				long double x, y, lengthxy;
+				x = getRandom();
+				y = getRandom();
+				lengthxy = sqrt(pow(x, 2.0)+ pow(y, 2.0));
+				if(lengthxy <= 1.0){
+					randomNumbersInside ++;
+				}
 			}
+			/*
+			 * Ratio of numbers inside and numbers outside is calculated and the
+			 * ratio is multiplied by four to get the approxamation of PI.
+			 */
+			fourthPI = (long double)randomNumbersInside / 
+				(long double)randomNumbers;
+			myPI = fourthPI * 4.0;
+
+			printf("Numbers inside: %llu \n", randomNumbersInside);
+
+			printf("\nVery inaccurate PI: %Lf \n", myPI);
 		}
-		fourthPI = (long double)randomNumbersInside / 
-			(long double)randomNumbers;
-		myPI = fourthPI * 4.0;
-
-		printf("Numbers inside: %llu \n", randomNumbersInside);
-
-		printf("\nVery inaccurate PI: %Lf \n", myPI);
+		else
+		{
+			printf("\nERROR:Number given below zero\n");
+		}
 	}
-	else{
-		printf("\nERROR:Number given below zero\n");
+	else
+	{
+		printf("\nERROR:No number given\n");
 	}
 
 	printf("\nWaiting for enter to exit:");
