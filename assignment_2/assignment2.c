7202716f362896b58911dfc13d9a7d0ed527b5e7
Snuggert <abe.wiersma@hotmail.nl>
2013-02-12 14:27:25 +0100

    assignment 2 done

---
+++ assignment_2/assignment2.c
@@ -0,0 +1,191 @@
+/*
+ * File: assignment1.c
+ * 
+ * This file contains a program in c to break the caesar style encryption.
+ *
+ * Author: Abe Wiersma (Snuggert)
+ * Date: 11-02-2013
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int getOffset(int min, int max);
+void printOffset(int offset, char *encryptedFile);
+
+int main( int argc, char *argv[] )
+{
+   if(argc == 3)
+   {
+      char ch;
+      char freqAlphabet[26][10];
+      char *encryptedFile = argv[1];
+      char *freqFile = argv[2];
+      // Make an array with a place to count a-z
+      int alphabet[26], index, freqIndex, numberOfChars, secondIndex, 
+         alphabetIndex, floatFlag, offset;
+      double percentage, mindifference, difference;
+      FILE *filePointer;
+
+      floatFlag = 0;
+      freqIndex = 0;
+      index = 0;
+      mindifference = 999;
+
+      // Empty the array
+      for(index = 0; index < 26; index++)
+      {
+         for(secondIndex = 0; secondIndex < 10; secondIndex++)
+         {
+            freqAlphabet[index][secondIndex] = 0;
+         }
+      }
+
+      // Create filepointer to the language frequency file
+      filePointer = fopen(freqFile,"r");
+
+      //check filepointer for validity
+      if( filePointer == NULL )
+      {
+         perror("Error while opening the file.\n");
+         exit(EXIT_FAILURE);
+      }
+
+      while( ( ch = fgetc(filePointer) ) != EOF )
+      {
+         if(ch == ' ')
+         {
+            floatFlag = 1;
+            index = 0;
+         }
+         else if(ch == '\n')
+         {
+            floatFlag = 0;
+         }
+         else if(floatFlag == 0)
+         {
+            freqIndex = (int)ch - 'a';
+            if(freqIndex < 0 || freqIndex > 25)
+            {
+               perror("Wrong file format in language frequency file.\n");
+               exit(EXIT_FAILURE);
+            }
+         }
+         else
+         {
+            freqAlphabet[freqIndex][index] = ch;
+            index ++;
+         }
+      }
+      fclose(filePointer);
+
+      printf("\nTrying offsets:\n");
+      for(offset = 0; offset < 26; offset++)
+      {
+         printf(" %d", offset);
+         // Create filepointer to the encrypted file.
+         filePointer = fopen(encryptedFile,"r");
+
+         //reset values
+         numberOfChars = 0;
+         difference = 0;
+         for(index = 0; index < 26; index++)
+         {
+            alphabet[index] = 0;
+         }
+
+         //check filepointer for validity
+         if( filePointer == NULL )
+         {
+            perror("Error while opening the file.\n");
+            exit(EXIT_FAILURE);
+         }
+
+         /*
+          * loop through file character by character every non space character 
+          * gets counted and the number of occurences of a character is kept.
+          */
+         while( ( ch = fgetc(filePointer) ) != EOF )
+         {
+            if(ch != ' ')
+            {
+               ch = (((ch -'a') + offset)%26) + 'a';
+               numberOfChars ++;
+               alphabetIndex = ch;
+               alphabetIndex -= 'a';
+               if(alphabetIndex < 0 || alphabetIndex > 25)
+               {
+                  perror("Wrong file format in encrypted file.\n");
+                  exit(EXIT_FAILURE);
+               }
+               alphabet[alphabetIndex] ++;
+            }        
+         }
+         fclose(filePointer);
+         for(index = 0; index < 26; index++)
+         {
+            percentage = ((double)alphabet[index]/(double)numberOfChars)*100;
+            difference += fabs(percentage - atof(freqAlphabet[index])); 
+         }
+         if(difference < mindifference)
+         {
+            printf("*");
+            mindifference = difference;
+         }
+      }
+
+      printf("\nDecoding with offset: ");
+      offset = getOffset(0, 26);
+      printOffset(offset, encryptedFile);
+      return 0;
+   }
+   else
+   {
+      printf("\nIncorrect way of calling the program.\n");
+      return 0;
+   }
+}
+
+int getOffset(int min, int max)
+{
+   int input;   
+   input = 0;
+
+   while (scanf("%d",&input) != 1 || input < min || input > max)
+   {
+      printf("Wrong input: ");
+   }
+
+   // while (getchar() != '\n'); <<<--- Should I put this here??
+
+   return input;
+}
+
+void printOffset(int offset, char *encryptedFile)
+{
+   char ch;
+   FILE *filePointer;
+
+   // Create filepointer to the encrypted file.
+   filePointer = fopen(encryptedFile,"r");
+
+   //check filepointer for validity
+   if( filePointer == NULL )
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   }
+
+   printf("\n");
+   // loop through the file and print every char.
+   while( ( ch = fgetc(filePointer) ) != EOF )
+   {
+      // if a char is not a space it will be set off by the offset given.
+      if(ch != ' ')
+      {
+         ch = (((ch -'a') + offset)%26) + 'a';
+      }
+      printf("%c", ch);
+   }
+   fclose(filePointer);
+}
--- test.c
+++ test.c
@@ -15,36 +15,6 @@ long double getRandom();
 
 int main( int argc, char *argv[] )
 {
-	int r, index, randomNumbersInside;
-	long randomNumbers;
-	long double fourthPI, myPI;
-
-	randomNumbers = atoi( (argv[1]) );
-	randomNumbersInside = 0;
-	r = 1;
-	printf("To be generated: %d \nwidth/height/radius: %d\n", randomNumbers, r);
-
-	for (index=0; index<randomNumbers; index++) {
-		long double x, y, lengthxy;
-		x = getRandom();
-		y = getRandom();
-		lengthxy = sqrt(pow(x, 2.0)+ pow(y, 2.0));
-		if(lengthxy <= 1.0){
-			randomNumbersInside ++;
-		}
-	}
-	printf("Numbers inside: %d \n", randomNumbersInside);
-	fourthPI = (long double)randomNumbersInside / (long double)randomNumbers;
-	myPI = fourthPI * 4.0;
-	printf("Very inaccurate PI %Lf \n", myPI);
-	printf("Waiting for enter to exit: \n");
-	getchar();
-
-	return 0;
-
-}
-
-long double getRandom ()
-{
-	return (long double)rand() / (long double)RAND_MAX;
+	char a = 'a';
+	printf("%c\n", ((a-'a' + 2)%26) + 'a');
 }
