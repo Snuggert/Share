d1b71a80bc2e8b76013d7cd2512ab809da73036f
Abe Wiersma <abe.wiersma@student.uva.nl>
2013-02-13 17:30:16 +0100

    minor change

--- assignment_2/assignment2.c
+++ assignment_2/assignment2.c
@@ -64,7 +64,7 @@ int main( int argc, char *argv[] )
          }
          else if(floatFlag == 0)
          {
-            freqIndex = (int)ch - 'a';
+            freqIndex = ch - 'a';
             if(freqIndex < 0 || freqIndex > 25)
             {
                perror("Wrong file format in language frequency file.\n");
